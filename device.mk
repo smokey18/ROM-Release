a253fb4bbf5c87001322ef21735dce305308e8fb
Smokey <kmboplays@gmail.com>
2020-05-21 10:49:25 +0000

    Update: 21 May 2020
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- device.mk
+++ device.mk
@@ -1,23 +1,16 @@
-Update: 15 May 2020
+Update: 21 May 2020
 ===================
 
-• Enable support for Pocket mode
-• QPower and Deep Sleep at the same time
-• Add overlays for SystemInfo
-• Fixed Smart charging issue
-• Add  CPU info overlay
-• update Power Profile from ysl
-• Adapt the exact values for lockscreen charging info
-• XiaomiParts: Add Speaker Gain
-• Xiaomiparts: Add Spectrum Tile
-• XiaomiParts: Fix USB Fastcharge
-• XiaomiParts: Add FPS info
-• XiaomiParts: Add Ambient Display Gestures
-• XiaomiParts: Digital-style icon for FPSInfo QS Tile
-• Use vulkan as default renderer
-• Fix dual sim devices
-• Disable perfd prop for now
-• Let's really fix the flicker issue this time
-• Use 8 icons in statusbar now
-• Revert back to stock fp
-some optimizations and performance boot as well...
+• Fix Reboot issue
+• Enable QPower and Deep sleep at the same time
+• Fix some Screen Glitches
+• Improve touch detection
+• Some Lau's magic props
+• Add some CAF props
+• Fix some display props
+• Improve UI
+• Fix Cast
+• Reverted Vulkan and Perfd
+• Improve some performance tweaks
+• Use MoeSyndrome Kernel as default
+and some optimaztions and rebasing...
