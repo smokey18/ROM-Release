00ceb51ec4a52b46b86b23db586e6325f62956b2
smokey18 <kmboplays@gmail.com>
2020-06-02 18:27:01 +0530

    Update: 02 June 2020
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- device.mk
+++ device.mk
@@ -1,16 +1,19 @@
-Update: 21 May 2020
+Update: 02 June 2020
 ===================
 
-• Fix Reboot issue
-• Enable QPower and Deep sleep at the same time
-• Fix some Screen Glitches
-• Improve touch detection
-• Some Lau's magic props
-• Add some CAF props
-• Fix some display props
-• Improve UI
-• Fix Cast
-• Reverted Vulkan and Perfd
-• Improve some performance tweaks
+• Whole device tree cleaned, done a lot of rebasing stuff
+• Provide module targets for libqmiservices
+• Update and modify wcnss_service
+• Force triple frame buffers now
+• Drop some offset properties
+• Drop unused camera firmware
+• Update CarrierConfig from LA.UM.8.6.r1-04400-89xx.0
+• Set partitions for loop device
+• Introduce 'SafailNet' ( Fix for CTS Bypass for some user's )
+• Improved Wifi Connection speed
+• Tune zram performance
+• Build kernel with latest Clang
+• Add GraphicBuffer shim for camera
+• Kang missing camera libs from Hydrogen
 • Use MoeSyndrome Kernel as default
-and some optimaztions and rebasing...
+and a whole lot more stuff, optimaztions and rebasing...
