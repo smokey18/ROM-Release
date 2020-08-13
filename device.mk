5f8be6d8558ca31decbe58db8fa93151ad2c9bee
Smokey <kmboplays@gmail.com>
2020-08-13 13:09:58 +0530

    Update: 14 August 2020
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- device.mk
+++ device.mk
@@ -1,12 +1,26 @@
-Update: 09 August 2020
+Update: 13 August 2020
 ===================
 
+• Added Pixel PowerHal with lots of improvements for mido
 • XiaomiParts: Add new Vibration Control
+• Import and Update data-ipa-cfg-mgr from LA.UM.8.6.2.r1-07800-89xx.0
+• Import audio, media and display hals from LA.UM.8.6.2.r1-08100-89xx.0
+• Update init.qcom.post_boot.sh from LA.UM.8.6.2.r1-08100-89xx.0
+• Update init.qcom.early_boot.sh from LA.UM.8.6.2.r1-08100-89xx.0
+• Import init.class_main.sh from LA.UM.8.6.2.r1-08100-89xx.0
 • Add dex2oat denials
 • Remove cpuset foreground boost
-• Improve scrolling responsiveness
-• Import and Update data-ipa-cfg-mgr from LA.UM.8.6.2.r1-07800-89xx.0
-
-
-Note:
-New Vibration Control is fully working fine, You might not feel the haptic vibration on changing vibration values but its actually change the Vibration.
+• Add highspeed recording profiles
+• Improve ZSL/HDR processing
+• Tune zram performance
+• Fix some camera related stuff
+• Reduce wifi battery drain
+• Clear settings cache after dirty flash
+• Fix display wakeup ( Maybe work )
+• Address lots of sepolicy denails and fixes
+• Fix freezing and lag issue
+• Improve scrolling response
+• Improve battery backup
+• Boost app opening speed
+• Better RAM Management
+and a lot of under the hood optimizations that i forgot to mention...
