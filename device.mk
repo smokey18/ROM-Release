1ada44dc71431591fb1760697a5143f3d94e0589
Smokey <kmboplays@gmail.com>
2020-08-17 22:27:28 +0500

    Update: 17 August 2020
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- device.mk
+++ device.mk
@@ -1,26 +1,9 @@
-Update: 13 August 2020
+Update: 17 August 2020
 ===================
 
-• Added Pixel PowerHal with lots of improvements for mido
-• XiaomiParts: Add new Vibration Control
-• Import and Update data-ipa-cfg-mgr from LA.UM.8.6.2.r1-07800-89xx.0
-• Import audio, media and display hals from LA.UM.8.6.2.r1-08100-89xx.0
-• Update init.qcom.post_boot.sh from LA.UM.8.6.2.r1-08100-89xx.0
-• Update init.qcom.early_boot.sh from LA.UM.8.6.2.r1-08100-89xx.0
-• Import init.class_main.sh from LA.UM.8.6.2.r1-08100-89xx.0
-• Add dex2oat denials
-• Remove cpuset foreground boost
-• Add highspeed recording profiles
-• Improve ZSL/HDR processing
-• Tune zram performance
-• Fix some camera related stuff
-• Reduce wifi battery drain
-• Clear settings cache after dirty flash
-• Fix display wakeup ( Maybe work )
-• Address lots of sepolicy denails and fixes
-• Fix freezing and lag issue
-• Improve scrolling response
-• Improve battery backup
-• Boost app opening speed
-• Better RAM Management
-and a lot of under the hood optimizations that i forgot to mention...
+• Optimize PowerHal nodes and other stuff
+• Update Powerhint values
+• Fix Hotspot
+• Fix Max and Min CPU Freq issue
+• Reverted some commits for fixing issues
+some other optimizations and fixes...
