dbe83f690f89cf5532c06300641dd14a179906af
Smokey <kmboplays@gmail.com>
2020-08-30 20:46:52 +0530

    Update: 30 August 2020
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- device.mk
+++ device.mk
@@ -1,9 +1,42 @@
-Update: 17 August 2020
+Update: 30 August 2020
 ===================
 
-• Optimize PowerHal nodes and other stuff
-• Update Powerhint values
-• Fix Hotspot
-• Fix Max and Min CPU Freq issue
-• Reverted some commits for fixing issues
-some other optimizations and fixes...
+• XiaomiParts: Add HAL3 | HAL1 Switch button profiles & QS Tiles
+• Enable VoLTE for Viettel & Mobifone Vietnam
+• Added VoLTE/VoWIFI status bar icon support
+• Battery & performance improvements
+• Fix OK Google + Internal Audio Recording
+• Added slow and fast charging threshold
+• Enable VoLTE support for Telkomsel (Indonesia)
+• Introduce LMKD
+• Enable PSI monitor
+• Use Pixel 4 CPU-Sets config
+• Disable WiFi RX Wakelock Feature
+• WiFi Smarter decisions on whether to use a 2GHz or 5Ghz AP
+• Clear the recovery cache in persist on boot
+• Add property value to indicate support for dual SIM mode ( need test )
+• Updated powerhal related stuff
+• Add new config overlay for call composer
+• Fixed WiFi random reboot issues when toggling on/off straight after boot up
+• SELinux fixes
+• Fix sim reset issue
+• IR/DRM/Widevine/WiFi converted to dynamic HALs
+• Build Dynamic Lifecycle DRM HAL
+• Switch to lazy WiFi HAL
+• Drop prebuilt HWC blobs
+• Merge gps_debug.conf into gps.conf
+• Add built-in mic and Built-In (Back) Mic to primary input sources
+• Restore previous wifi implementation
+• Risable HotwordService
+• Revert "mido: move wcnss-service to late_start"
+• Add missing compress-offload-playback bt-sco-wb sub paths
+• Prevent bt logspam
+• Fixing reset of selected preffered mobile data sim
+• Allow battery health to be read
+• Faster app opening
+• Use Cloudflare DNS servers for better internet quality
+• Set a lower screenrecording quality
+• Update qti-whitelist.xml from LA.UM.8.6.r1-04700-89xx.0
+• Improve RAM Management
+• Update & address additional sepolicy denials
+and many more under the hood optimizations...
